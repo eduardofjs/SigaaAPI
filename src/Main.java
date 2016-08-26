ea54b50e9c57389ae2f61633bc2c0d626dd13594
Joel Felipe Ferreira Gomes <joelfelipe07@gmail.com>
2016-08-25 21:40:56 -0300

    Commit Initial

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import br.ufrn.telefoneme.connection.OltuJavaClient;
+import br.ufrn.telefoneme.dto.TelefoneDTO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = OltuJavaClient.getResource("");
+        ArrayList<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
+        if(!text.equalsIgnoreCase("")){
+            try {
+                JSONArray array = new JSONArray(text);
+                for(int i = 0; i < array.length(); ++i){
+                    JSONObject jsonList = array.getJSONObject(i);
+
+                    telefones.add(new TelefoneDTO(jsonList.getString("numero"),
+                            jsonList.getString("descricao"),
+                            jsonList.getString("localizacao"),
+                            jsonList.getString("setor")));
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        for(TelefoneDTO telefone : telefones){
+        	System.out.println(telefone.getNumero());
+        }
+        
+	}
+
+}
