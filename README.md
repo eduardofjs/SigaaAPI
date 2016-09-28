ca6ffc47703a0ae8663039e3ca250c55963134df
marcielleal <marcielmanoel15@gmail.com>
2016-09-28 03:57:22 -0300

    README

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 #SigaaAPI
 
-###Projeto que visa organizar melhor os horários dos componentes curriculares da UFRN
+###Sobre
+* Projeto que visa distribuir, de forma mais organizada, as turmas que serão ofertadas em um determinado semestre na UFRN (Universidade Federal do Rio Grande do Norte)
+ 
+###Instruções de uso
+* É necessário compilar o projeto. Para isso basta fazer download e incluir no build path os arquivos .jar existentes na pasta raiz do projeto
+
+###Requerimentos
+* JDK 1.8
+
+###Colaboradores
+* 
+* Marciel Leal - marcielmanoel15@gmail.com
--- src/br/ufrn/telefoneme/test/StringToComponentTest.java
+++ src/br/ufrn/telefoneme/test/StringToComponentTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import br.ufrn.telefoneme.auxiliarhorario.Manha;
+import br.ufrn.telefoneme.auxiliarhorario.Turno;
+import br.ufrn.telefoneme.componente.Componente120h;
 import br.ufrn.telefoneme.conversion.StringToComponente;
 import br.ufrn.telefoneme.dto.ComponenteCurricularDTO;
 import br.ufrn.telefoneme.exception.ConexaoException;
@@ -15,7 +18,7 @@ public class StringToComponentTest {
 	private StringToComponente conv=new StringToComponente();
 	@Test
 	public void testGetComponentes() throws IdException, JsonStringInvalidaException, ConexaoException {
-		//
+		
 	}
 
 }
